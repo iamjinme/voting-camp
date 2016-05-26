68d6705585285a7c7abc87b7c9fa41fa33d5b2b5
Jinme Mirabal <mirabalj@gmail.com>
2016-05-26 13:40:46 -0400

    Add routes to login with Passport

--- app/routes/index.js
+++ app/routes/index.js
@@ -7,10 +7,19 @@ module.exports = function (app) {
   //var fileUpload = new FileUpload();
   
   app.route('/')
-      .get(function (req, res) {
-          res.sendFile(process.cwd() + '/public/index.html');
-      });
+    .get(function (req, res) {
+      res.sendFile(process.cwd() + '/public/index.html');
+    });
+      
+  app.get('/login/twitter',
+    passport.authenticate('twitter'));
 
+  app.get('/login/twitter/return', 
+    passport.authenticate('twitter', { failureRedirect: '/' }),
+    function(req, res) {
+      res.redirect('/');
+    });
+  
   //app.route('/upload')
   //    .post(fileUpload.getInfo);
 
