e66d9e7c2d16b7ffdcd113988c637016d1eed92a
Jinme Mirabal <mirabalj@gmail.com>
2016-05-26 21:24:53 -0400

    Logout and profile routes created

--- app/routes/index.js
+++ app/routes/index.js
@@ -8,7 +8,6 @@ module.exports = function (app, passport) {
 
   app.route('/')
     .get(function (req, res) {
-      console.log(req.user);
       res.render('index', { title: 'Voting Camp :: Back End Basejump FreeCodeCamp', user: req.user});
       //res.sendFile(process.cwd() + '/public/index.html');
     });
@@ -16,13 +15,22 @@ module.exports = function (app, passport) {
   app.get('/login/twitter',
     passport.authenticate('twitter'));
 
-  app.get('/login/twitter/return', 
+  app.get('/login/twitter/return',
     passport.authenticate('twitter', { failureRedirect: '/' }),
     function(req, res) {
       res.redirect('/');
     });
-  
-  //app.route('/upload')
-  //    .post(fileUpload.getInfo);
+
+  app.get('/logout',
+    function(req, res) {
+      req.session.destroy();
+      res.redirect('/');
+    });
+
+  app.get('/profile',
+    require('connect-ensure-login').ensureLoggedIn(),
+    function(req, res){
+      res.render('profile', { user: req.user });
+    });
 
 };
