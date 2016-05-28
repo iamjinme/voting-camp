d89e6710e72eda79d1722c5c81ecdc48e027c054
Jinme Mirabal <mirabalj@gmail.com>
2016-05-28 17:53:52 -0400

    Add function for submit votes

--- app/controllers/clickController.client.js
+++ app/controllers/clickController.client.js
@@ -8,6 +8,28 @@ $(document).ready(function() {
   $('#more').click(function() {
     $('#options').append('<input type="text" name="options[]" placeholder="...and more" class="form-input">');
   });
+  // Submit Form :: Vote Poll
+  $('#vote_poll')
+  .submit(function(e) {
+    e.preventDefault();
+    console.log('vote_poll');
+    var data = $(this).serialize();
+    console.log($('select').val());
+    if ($('select').val() !== '') {
+      $.ajax({
+        url: '/api/vote',
+        type: 'POST',
+        data: data
+      }).done(function(json){
+        //$('div.hide').removeClass('hide');
+        //$('#message').html(' Great, you have created poll "' + json.title + '"');
+        //$('#go_poll').attr('href', '/poll/' + json.hash);
+        //$('#new_poll').addClass('hide');
+        console.log(json);
+      });
+    };
+    e.preventDefault();
+  });
   // Submit Form :: New Poll
   $('#new_poll')
   .submit(function(e) {
